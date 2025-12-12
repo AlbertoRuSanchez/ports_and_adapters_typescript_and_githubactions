e564887fa1b0a7ac669571c0b57e3437ce6b9a02
albertors <albertorusanchez@gmail.com>
2025-12-12 13:21:16 +0100

    refactor: change email to be a valueobject and add unit tests

--- application/domain/User/User.ts
+++ application/domain/User/User.ts
@@ -1,12 +1,13 @@
 import { UserId } from './UserId';
+import { Email } from './Email';
 
 export class User {
 
     id : UserId;
     name : string;
-    email : string;
+    email : Email;
 
-    constructor(id: UserId, name: string, email: string) {
+    constructor(id: UserId, name: string, email: Email) {
         this.id = id;
         this.name = name;
         this.email = email;
