dc2e19a61bef296321115e518db5a57eda2f0e52
albertors <albertorusanchez@gmail.com>
2025-12-16 13:12:51 +0100

    fix: Correct relative import paths for `ServiceContainer` and `TaskLoadPort`.

--- src/Task/infrastructure/TaskExpressRouter.ts
+++ src/Task/infrastructure/TaskExpressRouter.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { TaskExpressController } from './primary/TaskExpressController';
-import { ServiceContainer } from '../../../Shared/infrastructure/ServiceContainer';
+import { ServiceContainer } from '../../Shared/infrastructure/ServiceContainer';
 
 const taskRouter = Router();
 const taskController = new TaskExpressController(ServiceContainer.task.taskLoadService);
